41f54a38a86f5a910ff95aa2312ff27049ad6ced
danieljacob-creator <danieljacob@ufl.edu>
2025-11-13 16:40:18 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,24 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
 # First example
-def add(a, b): 
-    pass
+def add(a, b): return a + b
 
+def subtract(a,b): return a - b
 
+def multiply(a,b): return a * b
+
+def divide(a,b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b/a
+
+def logarithm(a,b):
+    assert type(a) == int
+    assert type(b) == int
+    if AssertionError:
+        raise ValueError("Must enter integers")
+    return math.log(a, b)
+
+def exponent(a,b): return a**b
